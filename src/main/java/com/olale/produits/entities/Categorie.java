281f6a1953705f036dfaf7598113df0e176a5794
olalesam <olalesam87@gmail.com>
2025-09-04 16:34:07 +0000

    rechercher un produit en utilisant la methode query et les parametres positioonel ou nomme

---
+++ src/main/java/com/olale/produits/entities/Categorie.java
@@ -0,0 +1,55 @@
+package com.olale.produits.entities;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Categorie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCat; 
+	@OneToMany(mappedBy = "categorie")
+	private List<Produit> produits;
+	private String nomCat; 
+	private String descriptionCat;
+	
+	public Categorie() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public List<Produit> getProduits() {
+		return produits;
+	}
+
+	public void setProduits(List<Produit> produits) {
+		this.produits = produits;
+	}
+	
+	public Long getIdCat() {
+		return idCat;
+	}
+	public void setIdCat(Long idCat) {
+		this.idCat = idCat;
+	}
+	public String getNomCat() {
+		return nomCat;
+	}
+	public void setNomCat(String nomCat) {
+		this.nomCat = nomCat;
+	}
+	public String getDescriptionCat() {
+		return descriptionCat;
+	}
+	public void setDescriptionCat(String descriptionCat) {
+		this.descriptionCat = descriptionCat;
+	}
+	
+	
+}
