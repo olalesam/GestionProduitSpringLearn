0c4a5955f497df59ff2b3539472348b4f2bd38eb
olalesam <olalesam87@gmail.com>
2025-09-18 12:10:43 +0000

    add endpoint changing category

--- src/main/java/com/olale/produits/restControllers/CategorieRESTController.java
+++ src/main/java/com/olale/produits/restControllers/CategorieRESTController.java
@@ -5,30 +5,47 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.olale.produits.repositories.CategorieRepository;
 import com.olale.produits.entities.Categorie;
+import com.olale.produits.entities.Produit;
 
-@RestController 
-@RequestMapping("/api/cat") 
+@RestController
+@RequestMapping("/api/cat")
 @CrossOrigin("*")
 public class CategorieRESTController {
-	@Autowired 
-	 CategorieRepository categorieRepository; 
-	  
-	 @RequestMapping(method=RequestMethod.GET) 
-	 public List<Categorie> getAllCategories() 
-	 { 
-	  return categorieRepository.findAll(); 
-	 } 
-	  
-	 @RequestMapping(value="/{id}",method = RequestMethod.GET) 
-	 public Categorie getCategorieById(@PathVariable("id") Long id) { 
-	  return categorieRepository.findById(id).get(); 
-	  } 
-	 
-	 
+	@Autowired
+	CategorieRepository categorieRepository;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categorie> getAllCategories() {
+		return categorieRepository.findAll();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Categorie getCategorieById(@PathVariable("id") Long id) {
+		return categorieRepository.findById(id).get();
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public Categorie createCategory(@RequestBody Categorie categorie) {
+		// S’assurer que l’ID est nul → Hibernate fait un INSERT
+		categorie.setIdCat(null);
+		return categorieRepository.save(categorie);
+	}
+
+	@PutMapping
+	public Categorie updateCategorie(@RequestBody Categorie categorie) {
+		if (!categorieRepository.existsById(categorie.getIdCat())) {
+
+			throw new RuntimeException("Catégorie avec ID " + categorie.getIdCat() + " non trouvée");
+		}
+		return categorieRepository.save(categorie);
+	}
+
 }
--- src/test/java/com/olale/produits/ProduitsApplicationTests.java
+++ src/test/java/com/olale/produits/ProduitsApplicationTests.java
@@ -14,119 +14,101 @@ import com.olale.produits.repositories.ProduitRepository;
 @SpringBootTest
 class ProduitsApplicationTests {
 
-	@Autowired 
-	private ProduitRepository produitRepository; 
-	
-	@Test 
-	public void testCreateProduit() { 
-		Produit prod = new Produit("PC HP",4000.500,new Date()); 
-		produitRepository.save(prod); 
-	} 
-	
-	@Test 
-	public void testFindProduit() 
-	{ 
-		Produit p = produitRepository.findById(1L).get();     
-		System.out.println(p); 
-	} 
-	@Test 
-	public void testUpdateProduit() 
-	{ 
-		Produit p = produitRepository.findById(1L).get(); 
-		p.setPrixProduit(1000.0); 
-		produitRepository.save(p); 
-	} 
-	
-	@Test 
-	public void testListerTousProduits() 
-	 { 
-		   List<Produit>  prods = produitRepository.findAll();   
-		   
-		   for (Produit p : prods) 
-		   { 
-			   System.out.println(p); 
-		   }   
-	  } 
-	
-	@Test 
-	 public void testDeleteProduit() 
-	  { 
-	   produitRepository.deleteById(1L);;  
-	  }
-	
-	@Test 
-	 public void testFindByNomProduit() 
-	  { 
-		List<Produit>  prods = produitRepository.findByNomProduit("iphone X"); 
-	   for (Produit p : prods) 
-	   { 
-	    System.out.println(p); 
-	   } 
-	   
-	  } 
-	
-	@Test 
-	public void testFindByNomProduitContains () 
-	{ 
-		List<Produit> prods=produitRepository.findByNomProduitContains("iphone"); 
-		for (Produit p : prods) 
-		{ 
-			System.out.println(p); 
-		} 
-	} 
-	
+	@Autowired
+	private ProduitRepository produitRepository;
+
 	@Test
-	public void testfindByNomPrix() 
-	{ 
-		List<Produit>  prods = produitRepository.findByNomPrix("iphone X", 1000.0); 
-		for (Produit p : prods) 
-		{ 
-			System.out.println(p); 
-		} 
-	} 
-	
-	@Test 
-	public void testfindByCategorie() 
-	{ 
-		Categorie cat = new Categorie(); 
-		cat.setIdCat(1L);    
-		List<Produit>  prods = produitRepository.findByCategorie(cat); 
-		for (Produit p : prods) 
-		{ 
-			System.out.println(p); 
-		} 
+	public void testCreateProduit() {
+		Produit prod = new Produit("PC HP", 4000.500, new Date());
+		produitRepository.save(prod);
 	}
-	
-	@Test 
-	public void findByCategorieIdCat() 
-	{    
-		List<Produit>  prods = produitRepository.findByCategorieIdCat(1L); 
-		for (Produit p : prods) 
-		{ 
-			System.out.println(p); 
-		} 
-	} 
-	
-	@Test 
-	 public void testfindByOrderByNomProduitAsc() 
-	  { 
-		List<Produit>  prods = produitRepository.findByOrderByNomProduitAsc();   
-	   for (Produit p : prods) 
-	   { 
-	    System.out.println(p); 
-	   } 
-	  }
-	
-	@Test 
-	 public void testTrierProduitsNomsPrix() 
-	  { 
-		List<Produit>  prods = produitRepository.trierProduitsNomsPrix();
-	   
-	    for (Produit p : prods) 
-	    { 
-	    	System.out.println(p); 
-	    } 
-	  } 
-	
+
+	@Test
+	public void testFindProduit() {
+		Produit p = produitRepository.findById(1L).get();
+		System.out.println(p);
+	}
+
+	@Test
+	public void testUpdateProduit() {
+		Produit p = produitRepository.findById(1L).get();
+		p.setPrixProduit(1000.0);
+		produitRepository.save(p);
+	}
+
+	@Test
+	public void testListerTousProduits() {
+		List<Produit> prods = produitRepository.findAll();
+
+		for (Produit p : prods) {
+			System.out.println(p);
+		}
+	}
+
+	@Test
+	public void testDeleteProduit() {
+		produitRepository.deleteById(1L);
+		;
+	}
+
+	@Test
+	public void testFindByNomProduit() {
+		List<Produit> prods = produitRepository.findByNomProduit("iphone X");
+		for (Produit p : prods) {
+			System.out.println(p);
+		}
+
+	}
+
+	@Test
+	public void testFindByNomProduitContains() {
+		List<Produit> prods = produitRepository.findByNomProduitContains("iphone");
+		for (Produit p : prods) {
+			System.out.println(p);
+		}
+	}
+
+	@Test
+	public void testfindByNomPrix() {
+		List<Produit> prods = produitRepository.findByNomPrix("iphone X", 1000.0);
+		for (Produit p : prods) {
+			System.out.println(p);
+		}
+	}
+
+	@Test
+	public void testfindByCategorie() {
+		Categorie cat = new Categorie();
+		cat.setIdCat(1L);
+		List<Produit> prods = produitRepository.findByCategorie(cat);
+		for (Produit p : prods) {
+			System.out.println(p);
+		}
+	}
+
+	@Test
+	public void findByCategorieIdCat() {
+		List<Produit> prods = produitRepository.findByCategorieIdCat(1L);
+		for (Produit p : prods) {
+			System.out.println(p);
+		}
+	}
+
+	@Test
+	public void testfindByOrderByNomProduitAsc() {
+		List<Produit> prods = produitRepository.findByOrderByNomProduitAsc();
+		for (Produit p : prods) {
+			System.out.println(p);
+		}
+	}
+
+	@Test
+	public void testTrierProduitsNomsPrix() {
+		List<Produit> prods = produitRepository.trierProduitsNomsPrix();
+
+		for (Produit p : prods) {
+			System.out.println(p);
+		}
+	}
 
 }
